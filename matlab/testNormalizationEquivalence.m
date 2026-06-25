6a0152136be3e640e542141c0f00b38fa5590db1
Giacomo Fregara <jack.freg@gmail.com>
2026-06-25 16:48:04 +0200

    refactor: reference is as long as simulation

--- matlab/testNormalizationEquivalence.m
+++ matlab/testNormalizationEquivalence.m
@@ -71,6 +71,11 @@ qu = (uNormMin .* uMax - uNormMax .* uMin) ./ (uMax - uMin);
 Dm = blkdiag(my * eye(na), mu * eye(nb));
 q = [qy * ones(na, 1); qu * ones(nb, 1)];
 
+% Compute constants for C code
+myInvDmDg = my * Dm \ Dg;
+qmyInvDmDg = -q' * myInvDmDg;
+myInvDmc0 = my * (Dm \ c0);
+qmyInvDmc0 = -q'*myInvDmc0;
 %% Yalmip setup
 yalmip('clear');
 
